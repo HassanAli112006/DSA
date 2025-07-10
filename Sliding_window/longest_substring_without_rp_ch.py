913f65b0d0fc969fa6e762f34f17a2163bcd9765
Hassan Ali <ha526779@gmail.com>
2025-07-10 11:59:16 +0500

    Trying out my skills with strings

---
+++ Sliding_window/longest_substring_without_rp_ch.py
@@ -0,0 +1,29 @@
+"""
+🔹 Problem: Longest Substring Without Repeating Characters
+🔁 Pattern: Sliding Window (Variable Size)
+⭐ Difficulty: Medium
+📘 Source: LeetCode #3 (https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+🎯 Goal: Return the length of the longest substring with all unique characters.
+"""
+# Output: 3
+# Explanation: The answer is "abc", which has length 3.
+
+
+
+s = "abcabcbb"
+def longest_unique_substring(string: str) -> int:
+    max_len = 0
+    seen = set()
+    n = len(string)
+    l = 0
+    for r in range(n):
+        while string[r] in seen:
+            seen.remove(string[l])
+            l += 1
+        seen.add(string[r])
+        max_len = max(max_len, r-l+1)
+    return max_len
+print(longest_unique_substring(s)) 
+
+
+
