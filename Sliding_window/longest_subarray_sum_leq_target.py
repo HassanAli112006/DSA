6e66ba4d8010134c57fa7de86c83f6ee56ca886b
Hassan Ali <ha526779@gmail.com>
2025-07-10 11:49:56 +0500

    Fixed bug in the program

--- Sliding_window/longest_subarray_sum_leq_target.py
+++ Sliding_window/longest_subarray_sum_leq_target.py
@@ -4,20 +4,26 @@
 ⭐ Difficulty: Medium
 📘 Source: Custom
 """
-
+# Bug fixed causing wrong execution
 arr = [1, 2, 1, 0, 1, 1, 0]
 target = 4
+
 def longest_subarray_with_sum_leq_target(number: list, tr: int) -> int:
     n = len(number)
     l = 0
     max_length = 0
     cur_sum = 0
+
     for r in range(n):
         cur_sum += number[r]
+
         while cur_sum > tr:
             cur_sum -= number[l]
-            length = r-l+1
-            max_length = max(max_length, length)
             l += 1
+
+        max_length = max(max_length, r - l + 1)
+
     return max_length
+
 print(longest_subarray_with_sum_leq_target(arr, target))
+
