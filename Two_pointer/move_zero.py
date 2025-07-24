427ace6a88d4c2b3fe5940a0042d095cc95afe21
Hassan Ali <ha526779@gmail.com>
2025-07-24 23:21:50 +0500

    Next day

--- Two_pointer/move_zero.py
+++ Two_pointer/move_zero.py
@@ -11,16 +11,22 @@ nums = [0,1,0,3,12]
 
 def move_zero(number: list[int]) ->  list[int]:
 
+    # tracks next position for inserting non zero
     insert_pos = 0
+
+    # for iterating the list and replace the non zero only with the initial tracker index(current_pos)
     for r in range(len(number)):
         if number[r] != 0:
             number[insert_pos] = number[r]
             insert_pos += 1
 
+    # for filling out remaining list with zeros
     while insert_pos < len(number):
         number[insert_pos] = 0
         insert_pos+=1
     return number
+
+
 print(move_zero(nums))
 
 
