3f69d17b6c917db348984c277618892bd5521733
Hassan Ali <ha526779@gmail.com>
2025-08-12 22:11:14 +0500

    First bad version practice

--- Binary_search/practice.py
+++ Binary_search/practice.py
@@ -2,23 +2,54 @@
 # target = 5
 # Output: 2
 
-nums = [1,3,5,6]
-target = 2
-# Output: 1
+# nums = [1,3,5,6]
+# target = 2
+# # Output: 1
 
-# Input: nums = [1,3,5,6], target = 7  
-# Output: 4
+# # Input: nums = [1,3,5,6], target = 7  
+# # Output: 4
 
-def index(number: list, tr: int) -> int:
+# def index(number: list, tr: int) -> int:
+#     l = 0
+#     r = len(number) - 1
+#     while l<=r:
+#         m = l + (r-l)//2
+#         if number[m] == tr:
+#             return m
+#         elif number[m] <= tr:
+#             l = m + 1
+#         else:
+#             r = m - 1
+#     return l
+# print(index(nums, target))
+
+
+
+"""""""""""""""""""""
+First Bad Version
+"""""""""""""""""""""
+
+
+n = 5
+bad_version = 4
+
+def is_bad(version):
+    return version >= bad_version
+
+def first_bad_version(n, bad):
     l = 0
-    r = len(number) - 1
-    while l<=r:
-        m = l + (r-l)//2
-        if number[m] == tr:
-            return m
-        elif number[m] <= tr:
-            l = m + 1
-        else:
+    r = n
+    bad = -1
+    while l <= r:
+        m = l+(r-l)//2
+        if is_bad(m):
+            bad = m
             r = m - 1
-    return l
-print(index(nums, target))
+        else:
+            l = m+1
+    return bad
+print(first_bad_version(n, bad_version))
+
+
+
+
